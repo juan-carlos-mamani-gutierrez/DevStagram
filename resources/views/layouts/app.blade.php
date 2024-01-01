dc9ca79cf3fc5a2358f6b89eac29bca5343d158a
juan-carlos-mamani-gutierrez <jiseroca65528@gmail.com>
2023-12-31 22:38:10 -0400

    login diseno

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
                 </h1>
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuenta</a>
                 </nav>
             </div>
         </header> 
@@ -29,7 +29,7 @@
             @yield('contenido')
         </main>
 
-        <footer class="text-center p-5 text-gray-500 font-bold">
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold">
             DevStagram - Todos los derechos reservados {{ now()->year }}
         </footer>   
 
